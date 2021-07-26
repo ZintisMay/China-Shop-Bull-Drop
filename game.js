abed46b87f04d8e98cd181ad203fe8be6e1aa55a
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-07-25 22:22:46 -0400

    display card grid

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const target = document.getElementById("target");
 
+const cardGrid = document.getElementById("cardGrid");
+
 const gameBoard = createGameBoard(15, 20);
 
 const shopCards = [
@@ -118,6 +120,7 @@ const bullCards = [
 ];
 
 displayGameBoard();
+displayGrid(shopCards[0]);
 
 function createGameBoard(sizeX, sizeY) {
   let board = [];
@@ -131,6 +134,7 @@ function createGameBoard(sizeX, sizeY) {
 function displayGameBoard() {
   console.log(gameBoard);
   target.innerHTML = "";
+  let allRows = [];
   gameBoard.forEach((row, rowIndex) => {
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
@@ -146,6 +150,52 @@ function displayGameBoard() {
       });
       rowDiv.append(cellDiv);
     });
-    target.append(rowDiv);
+    allRows.push(rowDiv);
   });
+  target.append(...allRows);
+}
+
+function displayGrid(gridData) {
+  //remove empty ending rows for GridData
+  let lastRow = gridData.reduce((lastRow, row, index) => {
+    if (row.includes(1)) {
+      return index;
+    } else {
+      return lastRow;
+    }
+  }, 0);
+  console.log(lastRow);
+  gridData = gridData.slice(0, lastRow + 1);
+
+  //remove empty ending columsn for Griddata
+  let lastCol = gridData.reduce((lastCol, row) => {
+    lastCol2 = row.reduce((lastCell, cell, col) => {
+      if (cell == 1) {
+        return col;
+      } else {
+        return lastCell;
+      }
+    }, 0);
+    if (lastCol < lastCol2) {
+      return lastCol2;
+    } else {
+      return lastCol;
+    }
+  }, 0);
+  console.log(lastCol);
+  gridData = gridData.map((row) => row.slice(0, lastCol + 1));
+
+  let gridCells = [];
+  gridData.forEach((row) => {
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    row.forEach((cell) => {
+      let cellDiv = document.createElement("div");
+      cellDiv.classList.add("cell");
+      cellDiv.innerHTML = cell;
+      rowDiv.append(cellDiv);
+    });
+    gridCells.push(rowDiv);
+  });
+  cardGrid.append(...gridCells);
 }
